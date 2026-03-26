bb0bf9d0dc7bd43c17ca30df67cbacda661bdfa6
mizterwaffles <nhillhouse04@gmail.com>
2026-03-26 01:30:17 -0600

    Added basic monster classes, randomized monster names, items, and levels, and also added randomization of monsters on the grid

--- Lab08.Main/Program.cs
+++ Lab08.Main/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Lab08;
+class Program
+{
+    public static void Main()
+    {
+        new Tests();
+
+        UserInterface _interface = new();
+
+        Console.Write("What type of world would you like to play? (obstacle, monster) ");
+        string type = _interface.WorldType(Console.ReadLine());
+        GameLoop game = new();
+        
+        if (type == "obstacle") game.ObstacleMap();
+        else if (type == "monster") game.MonsterFighter();
+    }
+}
